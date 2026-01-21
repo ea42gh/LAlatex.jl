247ca8998211a6f15ec3de38822614ab50205eed
ea42gh <ea42_github@mail.com>
2026-01-21 08:53:08 -0500

    removed old examples

--- examples/matrix_basic.jl
+++
@@ -1,4 +0,0 @@
-using LAlatex
-
-A = [1 2 3; 4 5 6]
-println(L_show("A = ", A))
--- examples/qr_layout.jl
+++
@@ -1,8 +0,0 @@
-using LAlatex
-using BlockArrays
-
-Q = [1 0; 0 1]
-R = [2 1; 0 3]
-qr_block = BlockArray([Q zeros(2, 2); zeros(2, 2) R], [2, 2], [2, 2])
-
-println(L_show("Q R = ", qr_block; arraystyle=:barray))
